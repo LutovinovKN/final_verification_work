ff03d9e23e7204a76608850e69e8e5258a77f44f
LutovinovKN <VesperLKN@mail.ru>
2022-09-29 19:38:16 +0300

    Начал выполнять код

--- final_verification_work.cs
+++ final_verification_work.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите массив строк через пробел, напрмер ( hello 2 world :-) ): ");
+string[] matrix = Console.ReadLine().Split();
+// 2 цикла, один для переключения слов в словаре, второй для счётчика элементов в слове
