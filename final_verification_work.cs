607053b491112e2a36b7370ec88e5e4295f73e5f
LutovinovKN <VesperLKN@mail.ru>
2022-09-30 16:43:19 +0300

    Выполнил задачу

--- final_verification_work.cs
+++ final_verification_work.cs
@@ -1,4 +1,53 @@
-﻿
-Console.WriteLine("Введите массив строк через пробел, напрмер ( hello 2 world :-) ): ");
-string[] matrix = Console.ReadLine().Split();
-// 2 цикла, один для переключения слов в словаре, второй для счётчика элементов в слове
+﻿//! Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+//! длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+//! либо задать на старте Выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+//! лучше обойтись исключительно массивами.
+//!     Примеры:
+//!     ["hello", "2", "world", ":-)"] => ["2", ":-)"] 
+//!     ["1234", "1567", "-2", "computer science"] > ["-2"] 
+//!     ["Russia", "Denmark", "Kazan"] -> []
+
+Console.WriteLine("Введите массив строк через пробел");
+string[] array = Console.ReadLine().Split();
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+Console.ReadKey(true);
+
+
+//? Более грубый вариант
+// Console.WriteLine("Введите массив строк через пробел, напрмер ( hello 2 world :-) ): ");
+// string[] arrayEnter = Console.ReadLine().Split();
+// string[] arrayExit = new string[arrayEnter.Length];
+
+// void SecondArray(string[] arrayEnter, string[] arrayExit)
+// {
+//     int count = 0;
+//     for (int i = 0; i < arrayEnter.Length; i++)
+//     {
+//         if (arrayEnter[i].Length <= 3)
+//         {
+//             arrayExit[count] = arrayEnter[i];
+//             count++;
+//         }
+//     }
+// }
+
+// void PrintArray(string[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write($"{array[i]}");
+//     }
+//     Console.WriteLine();
+// }
+// SecondArray(arrayEnter, arrayExit);
+// PrintArray(arrayExit);
